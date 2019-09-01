67b055badccc44a1aada110ed90627143b546432
Thanh <khanhthy@yahoo.com>
2019-09-01 12:13:35 -0700

    Add Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/header';
 import './app.scss';
+import Headline from './components/headline';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="main">
+        <Headline header='POST' desc='Click the button to render text'/>
+      </section>
     </div>
   );
 }
